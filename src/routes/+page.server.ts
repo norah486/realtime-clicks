02d2c2ab3e9970dc1efab9159333fe3098714cb3
norah-st <32691116+norah486@users.noreply.github.com>
2023-07-24 02:50:11 -0400

    Release the shop

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -36,6 +36,82 @@ export const actions = {
             .eq('id', 1)
 
         }
+    },
+
+    buy_w_clicks: async ({ request }) => {
+        const fdata = await request.formData();
+
+        let name = fdata.get("codename");
+
+        const { data: shopData, error: shopErr } = await supabaseClient
+        .from('shop')
+        .select()
+        .eq('code_name', name)
+        
+        const { data: clickData, error: clickErr } = await supabaseClient
+        .from('instant-data')
+        .select()
+
+        if (clickData?.at(0)?.clicks >= shopData?.at(0)?.cost) {
+
+            const { error: clicksErr } = await supabaseClient
+            .from('instant-data')
+            .update({
+                clicks: clickData?.at(0)?.clicks - shopData?.at(0)?.cost
+            })
+            .eq('id', 1)
+
+            const { error: updErr } = await supabaseClient
+            .from('shop')
+            .update({
+                amount: shopData?.at(0)?.amount + 1,
+                cost: shopData?.at(0)?.cost + Math.ceil((1000 * ( shopData?.at(0)?.amount + 1 ) * 0.75 ))
+            })
+            .eq('code_name', name)
+
+        }
+
+        
+    },
+
+    buy_w_spent: async ({ request }) => {
+        const fdata = await request.formData();
+
+        let name = fdata.get("codename");
+
+        const { data: shopData, error: shopErr } = await supabaseClient
+        .from('shop')
+        .select()
+        .eq('code_name', name)
+        
+        const { data: clickData, error: clickErr } = await supabaseClient
+        .from('instant-data')
+        .select()
+
+        if (clickData?.at(0)?.spent_clicks >= shopData?.at(0)?.cost) {
+
+            const { error: clicksErr } = await supabaseClient
+            .from('instant-data')
+            .update({
+                spent_clicks: clickData?.at(0)?.spent_clicks - shopData?.at(0)?.cost
+            })
+            .eq('id', 1)
+
+            const { error: updErr } = await supabaseClient
+            .from('shop')
+            .update({
+                amount: shopData?.at(0)?.amount + 1,
+                cost: shopData?.at(0)?.cost + Math.ceil((1000 * ( shopData?.at(0)?.amount + 1 ) * 0.75 ))
+            })
+            .eq('code_name', name)
+
+        }
+
+        
+    },
+
+    ascend: async ({ request }) => {
+        console.log("You shouldn't be here right now.")
     }
 
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,15 +2,27 @@
     import { enhance } from "$app/forms";
     import { supabaseClient } from "$lib/supabase";
 
-    let number: Number;
+    let number: number;
     let multiplier: number = 1;
-    let spentClicks: Number = 0;
-    let message: String|null;
+    let spentClicks: number = 0;
+    let message: String|null = null;
+
+    interface ShopItems {
+        id: number,
+        code_name: string,
+        item_name: string,
+        cost: number,
+        amount: number,
+        image: string
+        uses: string
+    }
+
+    let shopItems: Array<Partial<ShopItems>>|null;
 
     async function getCurrent() {
         const { data, error } = await supabaseClient
         .from('instant-data')
-        .select("clicks, multiplier, spent_clicks, message")
+        .select()
 
         if (data?.at(0)?.clicks != null) {
             number = data?.at(0)?.clicks;
@@ -27,7 +39,17 @@
         message = data?.at(0)?.message;
     }
 
+    async function getShop() {
+        const { data, error } = await supabaseClient
+        .from('shop')
+        .select()
+        .order('id', { ascending: true })
+
+        shopItems = data
+    }
+
     let promise = getCurrent();
+    let shopPromise = getShop();
     
     supabaseClient
         .channel('any')
@@ -36,6 +58,42 @@
                 multiplier = payload.new.multiplier
                 spentClicks = payload.new.spent_clicks
                 message = payload.new.message
+            })
+        .subscribe()
+
+    supabaseClient
+        .channel('any1')
+        .on('postgres_changes', { event: 'UPDATE', schema: 'public', table: 'shop' }, payload => {
+                
+                if (shopItems != null) {
+
+                    const newArray = shopItems.slice(); // create a copy of the old array
+                    const newItem = payload.new; // replace with the actual new item
+                    const index = newArray.findIndex(item => item.id === newItem.id); // find the index of the object with the specified id
+                    if (index !== -1) {
+                        shopItems[index] = newItem; // replace the object at the found index with the new one
+                    }
+
+                }
+
+            })
+        .subscribe()
+    
+    supabaseClient
+        .channel('any2')
+        .on('postgres_changes', { event: 'INSERT', schema: 'public', table: 'shop' }, payload => {
+                
+                shopPromise = getShop();
+
+            })
+        .subscribe()
+
+    supabaseClient
+        .channel('any3')
+        .on('postgres_changes', { event: 'DELETE', schema: 'public', table: 'shop' }, payload => {
+                
+                shopPromise = getShop();
+
             })
         .subscribe()
 
@@ -49,17 +107,27 @@
             {#await promise}
                 <p class="py-6">Fetching number...</p>
             {:then}
-                <form method="POST" use:enhance>
-                    <div class="flex">
-                        <p class="flex flex-1 py-6 text-lime-500">{number}</p>
-                        <p class="flex flex-1 justify-end py-6 text-emerald-500">{spentClicks}</p>
-                    </div>
+    
+                <p class="text-lime-500 py-2">Clicks: {number}</p>
+                <p class="text-emerald-500">Spent Clicks: {spentClicks}</p>
 
-                    <div class="flex">
-                        <button class="btn btn-primary flex flex-1" formaction="?/updateClicks">Click Me</button>
-                        <button class="flex flex-1 btn btn-error" formaction="?/spendClicks">Spend 100 clicks</button>
+
+                <form method="POST" use:enhance>
+                    <div class="flex py-2">
+                        <button class="btn btn-primary flex flex-1" formaction="?/updateClicks">Click</button>
+
+                        {#if number < 100}
+                            <button class="flex flex-1 btn btn-error btn-disabled">Spend 100 clicks</button>
+                        {:else}
+                            <button class="flex flex-1 btn btn-error" formaction="?/spendClicks">Spend 100 clicks</button>
+                        {/if}
                     </div>
                 </form>
+
+                <div class="flex flex-1 justify-items-center justify-center py-3">
+                    <button class="btn btn-warning btn-disabled justify-center self-center align-middle justify-items-center" formaction="?/ascend">Let the world see</button>
+                </div>
+
                 {#if multiplier < 1}
                     <p class="text-sm py-2 text-sky-500">Multiplier: x{multiplier}</p>
                 {:else if multiplier == 1}
@@ -73,6 +141,50 @@
                 {#if message != null}
                     <p class="py-2 font-bold font-mono text-xl text-fuchsia-300">{message}</p>
                 {/if}
+
+                
+                <div class="hero-content max-w-full w-full flex-wrap flex flex-1">
+                    {#await shopPromise}
+                        <h1 class="font-bold">Fetching shop...</h1>
+                    {:then}
+                        {#if shopItems != null}
+                            {#each shopItems as s}
+
+                                <form method="POST" use:enhance>
+
+                                    <div class="card card-compact bg-base-200 shadow-xl w-96">
+                                        <div class="card-body">
+                                            <h2 class="card-title">{s.item_name}</h2>
+                                            <p>Cost: {s.cost}</p>
+                                            <p>Current amount: {s.amount}</p>
+                                            <div class="card-actions justify-end">
+                                                {#if s.uses == "clicks"}
+                                                    {#if number < (s.cost || 0)}
+                                                        <button class="btn btn-primary btn-disabled">Buy with Clicks</button>
+                                                    {:else}
+                                                        <button class="btn btn-primary" formaction="?/buy_w_clicks">Buy with Clicks</button>
+                                                    {/if}
+                                                {:else if s.uses == "spent"}
+                                                    {#if spentClicks < (s.cost || 0)}
+                                                        <button class="btn btn-primary btn-disabled">Buy with Spent Clicks</button>
+                                                    {:else}
+                                                        <button class="btn btn-primary" formaction="?/buy_w_spent">Buy with Spent Clicks</button>
+                                                    {/if}
+                                                {/if}
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <input type="hidden" name="codename" value="{s.code_name}">
+
+                                </form>
+                                
+                            {/each}
+                        {/if}
+                    {/await}
+                </div>
+                
+
             {/await}
         </div>
     </div>
